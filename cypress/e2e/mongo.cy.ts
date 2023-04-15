6f7f68dc8100b9a97268bf3cc62f16962ef9b391
themefju <98777676+themefju@users.noreply.github.com>
2023-04-15 12:30:22 +0200

    update findOne mongo tests

--- cypress/e2e/mongo.cy.ts
+++ cypress/e2e/mongo.cy.ts
@@ -6,51 +6,61 @@ it('works', () => {
 });
 
 context('findOne', () => {
-  it('works', () => {
-    findOne({
-      query: { framework: 'Cypress' },
-      collection: 'api-tests',
-    }).then((r) => {
-      expect(r).to.not.be.null;
-    });
-  });
-
   it('returns null', () => {
     findOne({
       query: { framework: '84302' },
       collection: 'api-tests',
-    }).then((r) => {
-      expect(r).to.be.null;
+    }).then((result) => {
+      expect(result).to.be.null;
+    });
+  });
+
+  it('works with _id = ObjectId', () => {
+    findOne({
+      query: { type: 'not nested ObjectId' },
+      collection: 'api-tests',
+    }).then((result) => {
+      expect(result).to.not.be.null;
+      expect(result.test).to.equal('ObjectId');
     });
   });
 
   it('works with _id = UUID', () => {
     findOne({
-      query: { framework: 1 },
+      query: { type: 'not nested UUID' },
       collection: 'api-tests',
-    }).then((r) => {
-      expect(r).to.not.be.null;
+    }).then((result) => {
+      expect(result).to.not.be.null;
+      expect(result.test).to.equal('UUID');
     });
   });
 
   it('works with nested _id = ObjectId', () => {
     findOne({
-      query: { framework: 'Cypress' },
+      query: { type: 'nested ObjectId' },
       collection: 'api-tests',
-    }).then((r) => {
-      findOne({ query: { _id: r['_id'] }, collection: 'api-tests' }).then(
-        (r) => expect(r).to.not.be.null
+    }).then((result) => {
+      findOne({ query: { _id: result['_id'] }, collection: 'api-tests' }).then(
+        (nestedResult) => {
+          expect(nestedResult).to.not.be.null;
+          expect(nestedResult.test).to.equal('ObjectId');
+          expect(nestedResult.type).to.equal('nested ObjectId');
+        }
       );
     });
   });
 
   it('works with nested _id = UUID', () => {
     findOne({
-      query: { framework: 1 },
+      query: { type: 'nested UUID' },
       collection: 'api-tests',
-    }).then((r) => {
-      findOne({ query: { _id: r['_id'] }, collection: 'api-tests' }).then(
-        (r) => expect(r).to.not.be.null
+    }).then((result) => {
+      findOne({ query: { _id: result['_id'] }, collection: 'api-tests' }).then(
+        (nestedResult) => {
+          expect(nestedResult).to.not.be.null;
+          expect(nestedResult.test).to.equal('UUID');
+          expect(nestedResult.type).to.equal('nested UUID');
+        }
       );
     });
   });
